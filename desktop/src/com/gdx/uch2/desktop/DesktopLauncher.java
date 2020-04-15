211efb4a2e600dea2dc8151b6a0d680615361328
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 03:25:29 +0200

    The start of a new era!

---
+++ desktop/src/com/gdx/uch2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.uch2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.uch2.UltimateChickenHorse2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UltimateChickenHorse2(), config);
+	}
+}
