8640fc5542b1399da39910b5830bd3b9d6e70fa2
BuildTools <unconfigured@null.spigotmc.org>
2020-04-16 02:44:49 +0200

    beginning of the main menu

---
+++ core/src/com/gdx/uch2/ui/MainMenu.java
@@ -0,0 +1,107 @@
+package com.gdx.uch2.ui;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MainMenu implements Screen {
+    private Stage stage;
+
+    public MainMenu(){
+        //parent = box2dTutorial;
+
+        /// create stage and set it as input processor
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {
+        // Create a table that fills the screen. Everything else will go inside this table.
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        // temporary until we have asset manager in
+        Skin skin = new Skin(Gdx.files.internal("neon/skin/neon-ui.json"));
+
+        // Create Image
+        Image chicken = new Image(new Texture(Gdx.files.internal(("chicken.png"))));
+
+        // Create Text
+        Label title = new Label("Ultimate Chicken Horse 2", skin);
+        title.setFontScale(2);
+        Label nickname = new Label("Nickname", skin);
+        Label joinRoom = new Label("Join Room", skin);
+        Label createRoom = new Label("Create Room", skin);
+
+        // Add to table
+        table.add(title).expandX().center();
+        table.add(chicken).width(129).height(200).center();
+        table.row();
+
+        //create buttons
+        TextButton join = new TextButton("Join", skin);
+        TextButton create = new TextButton("Create", skin);
+        TextButton quit = new TextButton("Quit", skin);
+
+        //add buttons to table
+        table.add(join).width(200).colspan(2);
+        table.row();
+        table.add(create).width(200).colspan(2);
+        table.row();
+        table.add(quit).width(200).colspan(2);
+
+        // create button listeners
+        quit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+    }
+
+    @Override
+    public void render(float delta) {
+        // clear the screen ready for next set of images to be drawn
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // tell our stage to do actions and draw itself
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
