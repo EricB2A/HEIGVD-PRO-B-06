183e55e1d627f2880da4b869a55259ea00c114bd
G-Houlmann <gildas.houlmann@gmail.com>
2020-04-16 16:49:01 +0200

    added network basis

---
+++ networking/src/main/java/com/uch2/networking/client/GameClient.java
@@ -0,0 +1,42 @@
+package com.uch2.networking.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class GameClient implements Runnable {
+    public void run(){
+        String host = "localhost";
+        int port = 12345;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try{
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                public void initChannel(SocketChannel ch) throws Exception{
+                    ch.pipeline().addLast(new GameClientHandler());
+                }
+            });
+
+            ChannelFuture f = b.connect(host, port).sync();
+
+            f.channel().closeFuture().sync();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ networking/src/main/java/com/uch2/networking/client/GameClientHandler.java
@@ -0,0 +1,29 @@
+package com.uch2.networking.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class GameClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf m = (ByteBuf) msg;
+        try {
+            while (m.isReadable()) {
+                System.out.print((char) m.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
