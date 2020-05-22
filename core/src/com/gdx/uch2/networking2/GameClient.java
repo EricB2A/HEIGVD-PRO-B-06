fc4bb00e2f5ae26dfa08545e694d2458994001ec
G-Houlmann <gildas.houlmann@gmail.com>
2020-05-23 01:28:23 +0200

    test client and server

--- core/src/com/gdx/uch2/networking2/GameClient.java
+++ core/src/com/gdx/uch2/networking2/GameClient.java
@@ -1,5 +1,10 @@
 package com.gdx.uch2.networking2;
 
+import com.gdx.uch2.networking.GameState;
+import com.gdx.uch2.networking.PlayerState;
+import com.gdx.uch2.networking2.serialization.MyInputStream;
+import com.gdx.uch2.networking2.serialization.MyOuputStream;
+
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -10,19 +15,30 @@ public class GameClient {
     public void connect(){
         try{
             Socket srv = new Socket("localhost", 12345);
-            DataInputStream is = new DataInputStream(new BufferedInputStream(srv.getInputStream()));
-            DataOutputStream os = new DataOutputStream(srv.getOutputStream());
+            MyInputStream in = new MyInputStream(srv.getInputStream());
+            MyOuputStream out = new MyOuputStream(srv.getOutputStream());
 
-            float f = 1000;
-            os.writeFloat(f);
-            os.flush();// send a byte to the server
-            f = is.readFloat();
 
-            System.out.println(f);
+            PlayerState ps1 = new PlayerState(1, 10, 20, System.nanoTime());
+            PlayerState ps2 = new PlayerState(2, 100, 200, System.nanoTime());
+
+            GameState gs = new GameState(new PlayerState[]{ps1, ps2});
+
+
+            System.out.println("Sending ps1");
+            out.writeMessage(ps1);
+            System.out.println("Sending ps2");
+            out.writeMessage(ps2);
+            System.out.println("Sending gs");
+            out.writeMessage(gs);
+
+            out.flush();
+
+            System.out.println("sent all and flushed");
 
             srv.close();
-            is.close();
-            os.close();
+            in.close();
+            out.close();
         }
         catch (Exception ex){
             ex.printStackTrace();
