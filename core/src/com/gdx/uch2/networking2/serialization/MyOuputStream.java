5b9794722286bcae86042450a5774009dd4d59b4
Eric Broutba <eric.bousbaa@hotmail.com>
2020-05-23 01:29:07 +0200

    Ajout outputStream implémentation

--- core/src/com/gdx/uch2/networking2/serialization/MyOuputStream.java
+++ core/src/com/gdx/uch2/networking2/serialization/MyOuputStream.java
@@ -1,7 +1,14 @@
 package com.gdx.uch2.networking2.serialization;
 
+import com.badlogic.gdx.Game;
+import com.gdx.uch2.networking.GameState;
+import com.gdx.uch2.networking.ObjectPlacement;
+import com.gdx.uch2.networking.PlayerState;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.concurrent.Semaphore;
 
 public class MyOuputStream {
@@ -13,11 +20,16 @@ public class MyOuputStream {
         this.stream = stream;
     }
 
-    public void writeMessage(MessageType messageType){
+    public void writeMessage(PlayerState playerState){
         try {
             mutex.acquire();
             try {
-                stream.writeChar(messageType.getChar());
+                // PlayerStateUpdate.
+                stream.writeChar(MessageType.PlayerStateUpdate.getChar());
+                stream.writeInt(playerState.getPlayerID());
+                stream.writeFloat(playerState.getPosX());
+                stream.writeFloat(playerState.getPosY());
+                stream.writeLong(playerState.getTime());
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -27,5 +39,49 @@ public class MyOuputStream {
             e.printStackTrace();
         }
     }
-    
+
+    public void writeMessage(GameState gameState){
+        try {
+            mutex.acquire();
+            try {
+                // GameStateUpdate.
+                stream.writeChar(MessageType.GameStateUpdate.getChar());
+                // Taille.
+                stream.writeInt(gameState.getPlayerStates().size());
+                Collection collection = gameState.getPlayerStates().values();
+                Iterator iterator = collection.iterator();
+                while(iterator.hasNext()){
+                    writeMessage((PlayerState) iterator.next());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mutex.release();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeMessage(ObjectPlacement objectPlacement){
+        try {
+            mutex.acquire();
+            try {
+                // BlockPlaced.
+                stream.writeChar(MessageType.BlockPlaced.getChar());
+                stream.writeInt(objectPlacement.getPlayerID());
+                // Bloc.
+                stream.writeInt(objectPlacement.getBlock().getType().ordinal());
+                stream.writeFloat(objectPlacement.getBlock().getPosition().x);
+                stream.writeFloat(objectPlacement.getBlock().getPosition().y);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mutex.release();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
