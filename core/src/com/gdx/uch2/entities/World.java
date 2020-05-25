bb3a7bec3de0dae90f68fe216b7e55c1db57f65b
Eric Broutba <eric.bousbaa@hotmail.com>
2020-05-25 23:26:34 +0200

    Major code hotfix

--- core/src/com/gdx/uch2/entities/World.java
+++ core/src/com/gdx/uch2/entities/World.java
@@ -33,7 +33,7 @@ public class World {
 
     public World(int noLevel) {
         createWorld(noLevel);
-        sound = Gdx.audio.newSound(Gdx.files.internal("sound/music.mp3"));
+        sound = Gdx.audio.newSound(Gdx.files.internal("sound/main_theme2.mp3"));
         sound.play();
     }
 
